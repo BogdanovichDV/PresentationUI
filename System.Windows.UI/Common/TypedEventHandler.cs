b4994d1606446a3b1501ce072de26ddab5d8c76f
Дмитрий <dimas1993ru@hotmail.com>
2022-01-24 23:37:59 +0500

    Added TypedEventHandler delegate.

---
+++ System.Windows.UI/Common/TypedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Windows;
+
+/// <summary>
+/// Представляет метод, который обрабатывает общие события.
+/// </summary>
+/// <typeparam name="TSender">
+/// Тип источника события.
+/// </typeparam>
+/// <typeparam name="TResult">
+/// Тип данных события.
+/// </typeparam>
+/// <param name="sender">
+/// Источник события.
+/// </param>
+/// <param name="args">
+/// Данные события. Если данные о событии отсутствуют, этот параметр будет иметь значение <see langword="null"/>.
+/// </param>
+public delegate void TypedEventHandler<TSender, TResult>(TSender sender, TResult args);
